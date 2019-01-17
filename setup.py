0f56815a5889bcb443d26db0d6978db3bcf773fa
jeffrey forman <jeff@jeffreyforman.net>
2019-01-17 09:28:31 -0500

    require either python 2.7 or not 3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pybindxml',
-    version='0.6.3',
+    version='0.7',
     description='Read ISC BIND\'s statistics XML for processing.',
     author='Jeffrey Forman',
     author_email='code@jeffreyforman.net',
@@ -10,4 +10,5 @@ setup(
     url='https://github.com/jforman/pybindxml',
     packages=['pybindxml'],
     install_requires=['beautifulsoup4'],
+    python_requires='>2.7, !=3.3.*',
 )
